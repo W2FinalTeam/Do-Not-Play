bee9aef50d3c4b3572537aa7394da03a59eda764
林怀颖 <58128350+linhuaiying@users.noreply.github.com>
2020-06-20 16:44:50 +0800

    Update MoveableItem.cs

--- MoveableItem.cs
+++ MoveableItem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +9,16 @@ public class MoveableItem : BaseItem, IMoveableItem
     private bool inHand;
     private GameObject role;
     public GameObject rightHandPositon;
+    AudioSource audio;
+    private Transform m;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
+        m = gameObject.GetComponent<Transform>();
+       
+        ThrowItem(true);
+        Invoke("PlayDropSound", 1f);
     }
 
     // Update is called once per frame
@@ -26,12 +32,12 @@ public class MoveableItem : BaseItem, IMoveableItem
         inHand = false;
         role = null;
     }
+    /// <summary>
+    /// 摧毁道具
+    /// </summary>
     protected override void Destory()
     {
         Destroy(this.gameObject);
-    }
-    protected override void ShowInfo()
-    {
     }
     public void Interact(GameObject target)
     {
@@ -40,13 +46,36 @@ public class MoveableItem : BaseItem, IMoveableItem
         PickUpItem(role);
     }
 
-    public void PlayDropSound()
+    public void PlayDropSound() //播放物品掉落的声音
     {
-            
+       audio.PlayOneShot((AudioClip)Resources.Load("物品掉落"));
     }
 
-    public void ThrowItem(bool inHand)
+    /// <summary>
+    /// 按下G键向人物前方投掷手中物品
+    /// </summary>
+    /// <param name="InHand">手中是否有物品</param>
+    public void ThrowItem(bool InHand) 
     {
+        if(InHand)
+        {
+            //首先判断是否为子物体
+            if (m.parent == this.transform)
+           
+            {
+                //让物品接受物理事件
+                this.GetComponent<Rigidbody>().isKinematic = false;
+                //运用TransformDirection()方法获取一个方向
+                Vector3 camDirct = m.TransformDirection(0, 3, 3);
+                //为物品添加一个向前的冲量
+                this.GetComponent<Rigidbody>().AddForce(camDirct, ForceMode.Impulse);
+            }
+            else
+            {
+                Debug.Log(m.position);
+                return;
+            }
+        }
         
     }
     //负责实现-拾取物品将物品展示在手上
@@ -64,4 +93,14 @@ public class MoveableItem : BaseItem, IMoveableItem
         }
 
     }
+  
+
+    /// <summary>
+    /// 当玩家靠近物品时显示按键UI
+    /// </summary>
+    public void ShowInfo()
+    {
+
+    }
+
 }
