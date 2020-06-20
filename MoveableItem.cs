2b5a2cbc324c9a7fcd7c6778712a64c5dfc7e7d1
林怀颖 <58128350+linhuaiying@users.noreply.github.com>
2020-06-21 00:40:55 +0800

    重构

--- MoveableItem.cs
+++ MoveableItem.cs
@@ -8,11 +8,11 @@ public class MoveableItem : BaseItem, IMoveableItem
     public Transform rightHandLocation;
     private bool inHand;
     private GameObject role;
-    private AudioSource audio;
+    private AudioSource clip;
     // Start is called before the first frame update
     void Start()
     {
-
+        clip = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -32,23 +32,23 @@ public class MoveableItem : BaseItem, IMoveableItem
     }
     public void PlayDropSound()
     {
-        audio.PlayOneShot((AudioClip)Resources.Load("物品掉落"));
+        clip.PlayOneShot((AudioClip)Resources.Load("物品掉落"));
     }
     /// <summary>
     /// 按下G键向人物前方投掷手中物品
     /// </summary>
     public void ThrowItem()
-    {
-        if (inHand)
-        {
-            transform.parent = null;
-            //让物品接受物理事件
-            this.GetComponent<Rigidbody>().isKinematic = false;
-            //运用TransformDirection()方法获取一个方向
-            Vector3 camDirct = transform.TransformDirection(0, 3, 3);
-            //为物品添加一个向前的冲量
-            this.GetComponent<Rigidbody>().AddForce(camDirct, ForceMode.Impulse);
-        }
+    {//不再放在手上
+        inHand = false;
+        //解除父子关系
+        this.transform.SetParent(null);
+        //让物品接受物理事件
+        this.GetComponent<Rigidbody>().isKinematic = false;
+        //运用TransformDirection()方法获取一个方向
+        Vector3 camDirct = transform.TransformDirection(0, 3, 3);
+        //为物品添加一个向前的冲量
+        this.GetComponent<Rigidbody>().AddForce(camDirct, ForceMode.Impulse);
+        Invoke("PlayDropSound", 0.8f);
     }
     //负责实现-拾取物品将物品展示在手上
     public void PickUpItem(GameObject role)
@@ -58,7 +58,9 @@ public class MoveableItem : BaseItem, IMoveableItem
         if (role.CompareTag("Child"))
         {
             transform.position = rightHandLocation.position;
+            transform.rotation = rightHandLocation.rotation;
             transform.parent = role.transform;
+            Debug.Log("hh");
         }
 
         if (role.CompareTag("Mother") || role.CompareTag("Father"))
