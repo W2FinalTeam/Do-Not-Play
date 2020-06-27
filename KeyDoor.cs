a3d110f50c3485928e83b7434fbdd6ebdf157093
林怀颖 <58128350+linhuaiying@users.noreply.github.com>
2020-06-27 15:30:51 +0800

    增加了装钥匙的柜门的触发事件

---
+++ KeyDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDoor : ChangeableItem
+{
+    public GameObject axe;//斧头
+    public Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void Interact()
+    {
+        if(player.inHandItem==axe)
+        {
+            this.GetComponent<Animator>().SetBool("isOpen_Obj_1", true);
+        }
+    }
+}
