93f5dc1807d740040348a55cd4d7368699112db0
林怀颖 <58128350+linhuaiying@users.noreply.github.com>
2020-06-27 15:30:07 +0800

    增加了猫的触发事件

---
+++ Cat.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : ChangeableItem
+{
+    public GameObject apple;
+    private bool isTrigger=false;
+    private AudioSource clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clip =gameObject.GetComponent<AudioSource>();
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void Interact()
+    {
+       if(isTrigger)
+        {
+            //停止播放猫叫
+            clip.enabled = false;
+            
+        }
+       else
+        {
+            return;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //开门以后，且旁边没有水果，开始播放猫叫
+        if(!isTrigger)
+        {
+            clip.enabled = true;
+            clip.Play();//循环播放
+        }
+        else
+        {
+            return;
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject==apple)
+        {
+            isTrigger = true; //苹果扔到猫旁边
+           
+           
+
+        }
+    }
+}
