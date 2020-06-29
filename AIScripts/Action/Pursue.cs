766a63d89f02f2e25630b9a9e65b80b3153bd166
xiaoqiansfather <2579327731@qq.com>
2020-06-29 23:04:23 +0800

    添加了关于行为树待实现的空脚本

---
+++ AIScripts/Action/Pursue.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+public class Pursue : Action 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AIScripts/Action/Rest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class Rest : Action 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
