227c1b41ead951e5612e46d4c8cab7fc7e8f170c
林怀颖 <58128350+linhuaiying@users.noreply.github.com>
2020-07-01 21:22:57 +0800

    修改了一些错误

--- KeyDoor.cs
+++ KeyDoor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class KeyDoor : ChangeableItem
 {
     public GameObject axe;//斧头
-    public Player player;
+   
     // Start is called before the first frame update
     void Start()
     {
